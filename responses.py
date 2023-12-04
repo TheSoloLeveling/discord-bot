074cede939e3dd5657b543cbac1949bc8e8cf09a
Bouziane Yassine <bouzianeyassine112@gmail.com>
2023-12-04 13:23:38 +0100

    first try discord bot

---
+++ responses.py
@@ -0,0 +1,13 @@
+def handle_response(message) -> str:
+    p_message = message.lower()
+
+    if p_message == 'hello':
+        return 'Hey souhail zamal'
+
+    if p_message == 'is souhail zamal ?':
+        return "YES"
+    
+    else :
+        return "cannot respond to that"
+
+        
